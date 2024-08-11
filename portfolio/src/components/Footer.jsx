67b27155e76016d72517e1a32365852675359ca6
sam <samuelmotari54@gmail.com>
2024-08-11 20:23:38 +0300

    hamburger

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,21 +1,31 @@
 export function Footer(){
     return (
         <>
-        <footer>
+        <footer className="footer">
             <a href="mailto:samuelmotari054@gmail.com" class="footer__link">samuelmotari054@gmail.com</a>
             <ul class="social-list">
-                <li class="social-list__item">
+            <li class="social-list__item">
                     <a class="social-list__link" href="https://codepen.io">
-                        a
+                        <i class="fab fa-codepen"></i>
                     </a>
                 </li>
                 
                 <li class="social-list__item">
                     <a class="social-list__link" href="http://dribbble.com">
-                        b
+                        <i class="fab fa-twitter"></i>
                     </a></li>
-                <li class="social-list__item"><a class="social-list__link" href="https://twitter.com">c</a></li>
-                <li class="social-list__item"><a class="social-list__link" href="https://github.com">d</a></li>
+
+                <li class="social-list__item">
+                    <a class="social-list__link" href="https://twitter.com">
+                        <i class="fab fa-github"></i>
+                    </a>
+                </li>
+
+                <li class="social-list__item">
+                    <a class="social-list__link" href="https://github.com">
+                    <i class="fab fa-linkedin-in"></i>
+                    </a>
+                </li>
             </ul>
         </footer>
         </>
